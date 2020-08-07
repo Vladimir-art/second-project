be60a732ec647a1e8deb9ca19b7f354710c6071a
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-08-07 18:03:35 +0300

    Create Footer

--- pictures/src/components/Api.js
+++ pictures/src/components/Api.js
@@ -26,7 +26,6 @@ class Api {
       method: 'GET'
     })
   }
-
 }
 
 export const api = new Api({
--- pictures/src/components/Footer.js
+++ pictures/src/components/Footer.js
@@ -3,18 +3,18 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
+      <nav className="footer__nav">
+        <ul className="footer__lists">
+          <li className="footer__list">Design free by:&#32;<a className="footer__link" target="_blank" href="http://slabdsgn.com/">@SLAB Design Studio</a></li>
+          <li className="footer__list">Developer&#32;<a className="footer__link" target="_blank" href="https://github.com/Vladimir-art">Vladimir Ermolaev</a></li>
+        </ul>
+      </nav>
       <div className="footer__info">
         <h3 className="footer__text">Ready to collaborate!</h3>
         <address className="footer__address">
-          Just shoot me an email at <a className="footer__link" href="vl.ermolaevv@yandex.ru">vl.ermolaevv@yandex.ru</a>
+          Just shoot me an email at <a className="footer__link" target="_blank" href="vl.ermolaevv@yandex.ru">vl.ermolaevv@yandex.ru</a>
         </address>
       </div>
-      <nav className="footer__nav">
-        <ul className="footer__lists">
-          <li className="footer__list">Design by:&#32;<a className="footer__link" href="http://slabdsgn.com/">@SLAB Design Studio</a></li>
-          <li className="footer__list">Developer&#32;<a className="footer__link" href="https://github.com/Vladimir-art">Vladimir Ermolaev</a></li>
-        </ul>
-      </nav>
     </footer>
   )
 }
